2c416eff32441d34f72a2bc904f48cb14ffdaed3
Gavrilo <ilicgavrilo80@gmail.com>
2018-02-25 14:55:08 +0100

    Proba example

--- Proba/1.js
+++ Proba/1.js
@@ -15,3 +15,18 @@ var c;
 console.log("d = "+ d);
 var d=12; //d=12; 
 console.log("d = "+ d);
+
+console.log(c);
+
+console.log(x);
+
+if (true) {
+    var m = 20;
+}
+console.log(m);
+var x = 18;
+console.log(x);
+
+console.log(n);
+var n;
+console.log(n);
