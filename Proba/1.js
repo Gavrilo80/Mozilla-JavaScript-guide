08de46f3d13b72886454f811ddfb20b262c45007
Gavrilo <ilicgavrilo80@gmail.com>
2018-02-25 11:00:12 +0100

    Proba example

---
+++ Proba/1.js
@@ -0,0 +1,17 @@
+var a = 5; //definicija
+var b; //deklaracija
+//var c;
+//var d; hoisting deklaracije promenljivih
+console.log("a= "+ a);
+console.log("b ="+ b);
+b=4;
+console.log("b= "+ b);
+a = 2;
+console.log("a= "+ a);
+
+console.log("c = "+ c);
+var c;
+
+console.log("d = "+ d);
+var d=12; //d=12; 
+console.log("d = "+ d);
